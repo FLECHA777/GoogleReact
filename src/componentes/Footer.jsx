a07d209b5028444c1ffbc62b72c1255f5a7e35ee
flecha <jonathanricardomachado@gmail.com>
2024-07-16 19:12:11 -0300

    concertando bugs

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -8,15 +8,15 @@ function Footer(){
             </div>
             <div className={Styles.footer2}>
                 <ul>
-                    <li><a>Sobre</a></li>
-                    <li><a>Publicidade</a></li>
-                    <li><a>Negócios</a></li>
-                    <li><a>Como funciona a Pesquisa</a></li>
+                    <li><a href=''>Sobre</a></li>
+                    <li><a href=''>Publicidade</a></li>
+                    <li><a href=''>Negócios</a></li>
+                    <li><a href=''>Como funciona a Pesquisa</a></li>
                 </ul>
                 <ul className={Styles.ulF}>
-                    <li><a>Privacidade</a></li>
-                    <li><a>Termos</a></li>
-                    <li><a>Configurações</a></li>
+                    <li><a href=''>Privacidade</a></li>
+                    <li><a href=''>Termos</a></li>
+                    <li><a href=''>Configurações</a></li>
                 </ul>
             </div>
         </footer>
