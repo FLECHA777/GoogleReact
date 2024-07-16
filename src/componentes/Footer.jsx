7e7667cc6cd16bfdc3b60413911de281307d5c9a
flecha <jonathanricardomachado@gmail.com>
2024-07-15 22:35:54 -0300

    finalização do prjeto

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -3,17 +3,17 @@ import Styles from './Footer.module.css'
 function Footer(){
     return(
         <footer className={Styles.footer}>
-            <div>
+            <div className={Styles.brasil}>
                 <p>Brasil</p>
             </div>
-            <div>
+            <div className={Styles.footer2}>
                 <ul>
                     <li><a>Sobre</a></li>
                     <li><a>Publicidade</a></li>
                     <li><a>Negócios</a></li>
                     <li><a>Como funciona a Pesquisa</a></li>
                 </ul>
-                <ul>
+                <ul className={Styles.ulF}>
                     <li><a>Privacidade</a></li>
                     <li><a>Termos</a></li>
                     <li><a>Configurações</a></li>
