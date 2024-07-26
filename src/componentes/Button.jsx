2c8c961f22df4ca0b61965b384c92f9a9c856c6e
flecha <jonathanricardomachado@gmail.com>
2024-07-26 15:18:20 -0300

    add componets

--- src/componentes/Button.jsx
+++ src/componentes/Button.jsx
@@ -1,12 +1,20 @@
 import Styles from './Button.module.css'
+import Check from './img/check.svg'
 
 function Button(){
 
     return(
-        <div className={Styles.btn}>
+      <div>
+         <div className={Styles.btn}>
           <button type="button">Pesquisa Google</button>  
           <button type="button">Estou com Sorte</button>  
         </div>
+        <div className={Styles.check}>
+          <img src={Check} alt="erro"/>
+          <a href="https://doodles.google/">Faça o Check-up de privacidade </a>
+        </div>
+      </div>
+       
     )
 }
 
